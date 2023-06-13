e819a65bd1b07f73eb8bf31129575336d03ffbd8
unknown <indiagauravkumar@gmail.com>
2023-06-13 08:36:51 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
-module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
-  images: {
-    domains: ['lh3.googleusercontent.com'],
-  },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    }
-    return config
-  }
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+      },
 }
 
 module.exports = nextConfig
+
+
